fa1b2214f16787793b033cf9faf1923817d8bffa
Tomas Zijdemans <113360400+tomas-zijdemans-vipps@users.noreply.github.com>
2023-12-18 10:32:57 +0100

    Cleanup headers (#4)
    * fix: Do not fill in acme headers
    
    * chore: Upgrade deps

--- sample_code/checkout_sample.ts
+++ sample_code/checkout_sample.ts
@@ -1,4 +1,4 @@
-import "https://deno.land/std@0.208.0/dotenv/load.ts";
+import "https://deno.land/std@0.209.0/dotenv/load.ts";
 import { Client } from "https://deno.land/x/vipps_mobilepay_sdk@0.1.0/mod.ts";
 
 // First, get your API keys from https://portal.vipps.no/
--- tests/test_deps.ts
+++ tests/test_deps.ts
@@ -2,5 +2,5 @@ export {
   assert,
   assertEquals,
   assertNotEquals,
-} from "https://deno.land/std@0.208.0/assert/mod.ts";
+} from "https://deno.land/std@0.209.0/assert/mod.ts";
 export * as mf from "https://deno.land/x/mock_fetch@0.3.0/mod.ts";
