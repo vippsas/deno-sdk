5804f5829725f692b1227c1abf2ecc778afee936
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-01-02 20:29:07 +0100

    fmt

--- tests/login_test.ts
+++ tests/login_test.ts
@@ -2,9 +2,12 @@ import { loginRequestFactory } from "../src/apis/login.ts";
 import { assertEquals } from "./test_deps.ts";
 
 Deno.test("login - discovery - should return the correct RequestData object", () => {
-    const requestData = loginRequestFactory.discovery();
-  
-    assertEquals(requestData.url, "/access-management-1.0/access/.well-known/openid-configuration");
-    assertEquals(requestData.method, "GET");
-    assertEquals(requestData.omitHeaders, ["Ocp-Apim-Subscription-Key"]);
-  });
+  const requestData = loginRequestFactory.discovery();
+
+  assertEquals(
+    requestData.url,
+    "/access-management-1.0/access/.well-known/openid-configuration",
+  );
+  assertEquals(requestData.method, "GET");
+  assertEquals(requestData.omitHeaders, ["Ocp-Apim-Subscription-Key"]);
+});
