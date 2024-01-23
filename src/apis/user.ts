79aafb3e12919c4a6bda298750779d5efb51a57b
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-01-23 14:05:59 +0100

    Implement Userinfo get

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,12 +1,13 @@
 import { RequestData } from "../types.ts";
+import { UserInfo, UserInfoError } from "./types/user_types.ts";
 
 /**
  * Factory object for creating Userinfo API requests.
  */
 export const userRequestFactory = {
-  info(): RequestData<unknown, unknown> {
+  info(sub: string): RequestData<UserInfo, UserInfoError> {
     return {
-      url: `/user...`,
+      url: `/vipps-userinfo-api/userinfo/${sub}`,
       method: "GET",
     };
   },
