2a86f936c2cc08a5682086bd8f6a925c362c6464
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-02-02 08:39:07 +0100

    comment

--- src/deps.ts
+++ src/deps.ts
@@ -10,8 +10,8 @@ export {
 export { parseMediaType } from "https://deno.land/std@0.212.0/media_types/parse_media_type.ts";
 export { filterKeys } from "https://deno.land/std@0.212.0/collections/mod.ts";
 /**
- * This is a workaround for the fact that the `crypto.randomUUID` function is 
- * not available in Node.js 18. This will be removed once Node.js 18 is
- * not supported anymore.
+ * This is a workaround for `crypto.randomUUID` not being available in 
+ * Node.js 18. This will be removed after Node.js 18 reaches End-of-Life 
+ * 30 Apr 2025.
  */
 export { v4 as uuid } from "https://deno.land/std@0.158.0/uuid/mod.ts";
