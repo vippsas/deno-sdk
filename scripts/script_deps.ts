fc3e18c2dbafa8cca45ae190a33dbe6eb45fb883
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-08-21 09:51:24 +0200

    Update std

--- scripts/script_deps.ts
+++ scripts/script_deps.ts
@@ -1,9 +1,9 @@
 export {
   greaterThan,
   parse,
-} from "https://deno.land/std@0.217.0/semver/mod.ts";
-export { Spinner } from "https://deno.land/std@0.217.0/cli/mod.ts";
-export * as colors from "https://deno.land/std@0.217.0/fmt/colors.ts";
-export { delay } from "https://deno.land/std@0.217.0/async/delay.ts";
+} from "https://deno.land/std@0.224.0/semver/mod.ts";
+export { Spinner } from "https://deno.land/std@0.224.0/cli/mod.ts";
+export * as colors from "https://deno.land/std@0.224.0/fmt/colors.ts";
+export { delay } from "https://deno.land/std@0.224.0/async/delay.ts";
 
 export { build, emptyDir } from "https://deno.land/x/dnt@0.40.0/mod.ts";
