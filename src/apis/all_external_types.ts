2b4af25a83c4d9c2fa9303e2c2fcc685fd83975b
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-09-10 14:49:04 +0200

    Fix exports

--- src/apis/all_external_types.ts
+++ src/apis/all_external_types.ts
@@ -1,7 +1,27 @@
 /**
  * Export all API types, for convenience. All exported types are
- * prefixed with the API name, to avoid potential naming conflicts.
+ * prefixed with the API name, to avoid naming conflicts.
  */
-export type * from "./types/auth_types.ts";
-export type * from "./generated_types/epayment/types.gen.ts";
-export type * from "./types/webhooks_types.ts";
+export type * as Auth from "./types/auth_types.ts";
+export type * as Webhooks from "./generated_types/webhooks/types.gen.ts";
+
+// Export all types from epayment
+export type {
+  CancelPaymentResponse,
+  CaptureModificationRequest,
+  CapturePaymentResponse,
+  CreatePaymentResponse,
+  ForceApprove,
+  ForceApproveResponse,
+  GetPaymentEventLogResponse,
+  GetPaymentResponse,
+  Problem,
+  RefundModificationRequest,
+  RefundPaymentResponse,
+} from "./generated_types/epayment/types.gen.ts";
+// Export CreatePaymentRequest from epayment, making the reference field optional
+import { CreatePaymentRequest as CreatePaymentRequestInternal } from "./generated_types/epayment/types.gen.ts";
+export type CreatePaymentRequest = CreatePaymentRequestInternal & {
+  reference?: string;
+};
+
