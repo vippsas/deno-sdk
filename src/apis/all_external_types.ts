42671b9ff0a74212403097fbd58e094ebf505c8c
Tomas Zijdemans <tomas.zijdemans@vipps.no>
2024-09-10 15:11:03 +0200

    Move Webhooks to generated types

--- src/apis/all_external_types.ts
+++ src/apis/all_external_types.ts
@@ -1,11 +1,16 @@
 /**
- * Export all API types, for convenience. All exported types are
+ * Re-export all API types, for convenience. All exported types are
  * prefixed with the API name, to avoid naming conflicts.
  */
 export type * as Auth from "./types/auth_types.ts";
-export type * as Webhooks from "./generated_types/webhooks/types.gen.ts";
 
-// Export all types from epayment
+export type {
+  ProblemDetails,
+  QueryResponse,
+  RegisterRequest,
+  RegisterResponse,
+} from "./generated_types/webhooks/types.gen.ts";
+
 export type {
   CancelPaymentResponse,
   CaptureModificationRequest,
@@ -19,9 +24,8 @@ export type {
   RefundModificationRequest,
   RefundPaymentResponse,
 } from "./generated_types/epayment/types.gen.ts";
-// Export CreatePaymentRequest from epayment, making the reference field optional
+
 import { CreatePaymentRequest as CreatePaymentRequestInternal } from "./generated_types/epayment/types.gen.ts";
 export type CreatePaymentRequest = CreatePaymentRequestInternal & {
   reference?: string;
 };
-
